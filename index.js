3ceb783dff9ef27c5e33c4255196b6f9e1e8ab03
Nicolangelo98 <nicolangeloricchetti98@gmail.com>
2024-06-18 11:12:47 +0200

    input dati

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ document.getElementById('sendRequest').addEventListener('click', async () => {
     const token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjY3NGRiYmE4ZmFlZTY5YWNhZTFiYzFiZTE5MDQ1MzY3OGY0NzI4MDMiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIzMjU1NTk0MDU1OS5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImF1ZCI6IjMyNTU1OTQwNTU5LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTAxMjIyMjQ2MjkzMDE2Mzg1ODI4IiwiaGQiOiJiYXplYXBwLml0IiwiZW1haWwiOiJkYXJpby52YWxhc3Ryb0BiYXplYXBwLml0IiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF0X2hhc2giOiJvMkZLUHYtRzFsbDZpUF90VVBVaG13IiwiaWF0IjoxNzE3NzY3NjMzLCJleHAiOjE3MTc3NzEyMzN9.UnvqLNXiK2nfAFDb3qnPpfYthR3z8bLLkkL_t1Jty8JkXMGQv44uLrSAbf60SuVdmXfxUXkmPICFbNzBYHTmDkGoT0fO11PzcUbd9dhj9gsXE4PVMSX0d-w6k_FIUXZmdQ2vYZKFOsiS5UmNDWfEE7dPno66iWku8An3sROnGqesqlM_th7H3bPzBUc7lBji5-Zl5sKbmXLlULvt2bZ8yfqzITR-5x_twjYFNe5gmNDeJMiiiLk8lGDH_mfe5G5fokhUocuuVRZa8GeSSrfglsCwgLdK3ytMmZuS48v8GsLQQm7wTLJREkc3wcJk756sEhBNZtsbZ0116bUOj8mYIA'; // Replace with the actual token
 
     const data = {
-        durataContratto: "indeterminato",
-        tipoContratto: "convivente",
-        livelloContratto: "A",
-        oreLunedi: "9",
-        oreMartedi: "4",
-        oreMercoledi: "2",
-        oreGiovedi: "0",
-        oreVenerdi: "0",
-        oreSabato: "0",
-        oreDomenica: "0",
+        durataContratto: duratacontrattoselezionato,
+        tipoContratto: tipocontrattoselezionato,
+        livelloContratto: livellocontrattoselezionato,
+        oreLunedi: document.getElementById("lunedi").value,
+        oreMartedi: document.getElementById("martedi").value,
+        oreMercoledi: document.getElementById("mercoledi").value,
+        oreGiovedi:document.getElementById("giovedi").value,
+        oreVenerdi: document.getElementById("venerdi").value,
+        oreSabato: document.getElementById("sabato").value,
+        oreDomenica: document.getElementById("domenica").value,
         paga: document.getElementById("Inputpaga").value,
         bambino_6_anni: "si",
         piu_persone: "no",
@@ -53,8 +53,7 @@ document.getElementById('sendRequest').addEventListener('click', async () => {
             'Specifiche vitto e alloggio': [],
             'Bambino entro 6 anni': null,
             'Certificato UNI1176': null,
-            'Autosufficienti':null,
-            'Inputpaga':0
+            'Autosufficienti':null
         };
 
         function toggleChoices(choiceId) {
@@ -154,15 +153,26 @@ document.getElementById('sendRequest').addEventListener('click', async () => {
                 document.getElementById('pagaImporto').innerText = paga+ ' €';
             }
 
-
-
+        let livellocontrattoselezionato = "";
+        let tipocontrattoselezionato = "";
+        let duratacontrattoselezionato = "";  
+        
         function selectChoice(category, choice) {
         selections[category] = choice;
         displaySelections();
+        if (category === 'Tipo contratto') {
+            tipocontrattoselezionato = selections['Tipo contratto'];
+        }
+        if (category === 'Livello') {
+            livellocontrattoselezionato = choice;
+        }
+        if (category === 'Contratto') {
+            duratacontrattoselezionato = choice;
+        }    
 
         if (category === 'Tipo contratto') {
          updateLevelChoices(choice);
-        } else if (category === 'Livello') {
+        } else if (category === 'Livello') {    
         if (choice === 'bs') {
             document.getElementById('bambinoBox').style.display = 'block';
         } else {
@@ -327,7 +337,7 @@ document.getElementById('sendRequest').addEventListener('click', async () => {
             document.getElementById('simulazione').style.display='none';
             document.getElementById('InputpagaBox').style.display='none';
             document.getElementById('pagabox').style.display='none';
-            document.getElementById('Inputpaga').value = "0";
+            document.getElementById('Inputpaga').value = "";
             
                 
 
